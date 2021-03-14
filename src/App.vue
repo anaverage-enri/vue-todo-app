2b56b256df982e274988fb199f515088532a4cef
Enri <ibenrique2510@gmail.com>
2021-03-14 17:24:10 +0700

    add vue route

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,19 @@
 <template>
   <div id="app">
     <Header/>
-    <AddTodo @add-todo="addTodo"/>
-    <Todos :todos="todos" @del-todo="deleteTodo"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 import Header from './components/layout/Header'
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(res => {
-          if (res.status === 200) this.todos = this.todos.filter(todo => todo.id !== id)
-        })
-        .catch(err => console.log(err))
-    },
-    addTodo(newTodo) {
-      const {title, completed} = newTodo;
-
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-        .then(res => this.todos.push(res.data))
-        //.then(res => this.todos = [...this.todos, res.data])
-        .catch(err => console.log(err))
-    }
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(res => this.todos = res.data)
-      .catch(err => console.log(err))
+    Header
   }
 }
 </script>
-
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-
-  .btn:hover {
-    background: #666;
-  }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>A small Todo Application for me to get the fundamentals of Vue.js</p>
+  </div>
+</template>
